d370289772d939eebeee40fb39a57ac00cb5db25
Isaiah <isaiahreimer98@gmail.com>
2019-07-31 23:08:48 -0400

    forgot file

---
+++ operators.h
@@ -0,0 +1,15 @@
+#ifndef __OPERATORS_H__
+#define __OPERATORS_H__
+
+// holds operater token numbers, since they are used both by lexer and other parts of the program.
+
+#define PLUS 1001
+#define DOUBLEEQUALS 1002
+#define LESS 1003
+#define GREATER 1004
+#define SUBTRACT 1005
+#define MULT 1006
+#define AMPERSAND 1007
+#define CARET 1008
+
+#endif
